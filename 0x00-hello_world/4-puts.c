ca4a8c5f04f71660db28f35c1699d2d5cbe3b257
Whisco <malaumonday@gmail.com>
2022-12-15 13:43:08 -0800

    cisfun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,2 @@
-# include <stdlo.h>
-
-int main(void)
-{
-  puts{"programming is like building a multilingual puzzle\n")
-    return (0);
-}
+#!/bin/bash
+int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
